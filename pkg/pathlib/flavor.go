44a2c99d652eb3f6ff7fa79f4882415b5775ddd0
Andre Lehmann <aisberg@posteo.de>
2023-03-05 13:41:42 +0100

    feat: implement windows flavored paths

--- pkg/pathlib/flavor.go
+++ pkg/pathlib/flavor.go
@@ -2,6 +2,7 @@ package pathlib
 
 import (
 	"strings"
+	"unicode/utf8"
 )
 
 type flavorer interface {
@@ -57,12 +58,12 @@ func (pf posixFlavor) HasDrive() bool {
 // SplitRoot splits the given path into a drive, root and relative path
 // component.
 func (pf posixFlavor) SplitRoot(path string) (string, string, string) {
-	if len(path) > 0 && path[0:1] == pf.Separator() {
-		stripped := strings.TrimLeft(path, pf.Separator())
+	if len(path) > 0 && path[0:1] == "/" {
+		stripped := strings.TrimLeft(path, "/")
 		if len(path)-len(stripped) == 2 {
-			return "", pf.Separator() + pf.Separator(), stripped
+			return "", "//", stripped
 		}
-		return "", pf.Separator(), stripped
+		return "", "/", stripped
 	}
 	return "", "", path
 }
@@ -83,6 +84,12 @@ func (pf posixFlavor) CasefoldParts(parts []string) []string {
 //
 // -----------------------------------------------------------------------------
 
+var (
+	windowsSeparator    = "\\"
+	windowsAltSeparator = "/"
+	windowsDriveLetters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+)
+
 // posixFlavor represents the Windows style path flavor.
 type windowsFlavor struct{}
 
@@ -93,12 +100,12 @@ func newWindowsFlavor() windowsFlavor {
 
 // Separator returns the separator of the flavor.
 func (wf windowsFlavor) Separator() string {
-	return "\\"
+	return windowsSeparator
 }
 
 // AltSeparator returns the alternative separator of the flavor.
 func (wf windowsFlavor) AltSeparator() string {
-	return "/"
+	return windowsAltSeparator
 }
 
 // HasDrive indicates whether the flavor has a drive component.
@@ -109,15 +116,76 @@ func (wf windowsFlavor) HasDrive() bool {
 // SplitRoot splits the given path into a drive, root and relative path
 // component.
 func (wf windowsFlavor) SplitRoot(path string) (string, string, string) {
-	// TODO: need to implement windows flavor
-	if len(path) > 0 && path[0:1] == wf.Separator() {
-		stripped := strings.TrimLeft(path, wf.Separator())
-		if len(path)-len(stripped) == 2 {
-			return "", wf.Separator() + wf.Separator(), stripped
+	var (
+		sep    = '\\'
+		prefix string
+	)
+	// check for extended-length path
+	if strings.HasPrefix(path, `\\?\`) {
+		if strings.HasPrefix(path[4:], `UNC\`) {
+			prefix = path[:7]
+			path = `\` + path[7:]
+		} else {
+			prefix = path[:4]
+			path = path[4:]
 		}
-		return "", wf.Separator(), stripped
 	}
-	return "", "", path
+	runes := []rune(path)
+	first := utf8.RuneError
+	if len(runes) > 0 {
+		first = runes[0]
+	}
+	second := utf8.RuneError
+	if len(runes) > 1 {
+		second = runes[1]
+	}
+	third := utf8.RuneError
+	if len(runes) > 2 {
+		third = runes[2]
+	}
+	if second == sep && first == sep && third != sep {
+		// is a UNC path:
+		// vvvvvvvvvvvvvvvvvvvvv root
+		// \\machine\mountpoint\directory\etc\...
+		//            directory ^^^^^^^^^^^^^^
+		index := runesIndexOffset(runes, sep, 2) // index of starting byte
+		if index != -1 {
+			index2 := runesIndexOffset(runes, sep, index+1)
+			// a UNC path can't have two slashes in a row
+			// (after the initial two)
+			if index2 != index+1 {
+				if index2 == -1 {
+					index2 = len(runes)
+				}
+				p := ""
+				if index2+1 <= len(runes) {
+					p = string(runes[index2+1:])
+				}
+				if prefix != "" {
+					return prefix + string(runes[1:index2]), string(sep), p
+				}
+				return string(runes[:index2]), string(sep), p
+			}
+		}
+	}
+	drv, root := "", ""
+	if second == ':' && strings.IndexRune(windowsDriveLetters, first) != -1 {
+		drv = string(runes[:2])
+		runes = runes[2:]
+		first = third
+	}
+	if first == sep {
+		root = string(first)
+		// trim left
+		i := 0
+		for ; i < len(runes); i++ {
+			if runes[i] != sep {
+				break
+			}
+		}
+		runes = runes[i:]
+	}
+	return prefix + drv, root, string(runes)
 }
 
 // Casefold returns the given string in a casefolded form.
@@ -131,4 +199,19 @@ func (wf windowsFlavor) CasefoldParts(parts []string) []string {
 		parts[i] = strings.ToLower(parts[i])
 	}
 	return parts
+}
+
+func runesIndexOffset(runes []rune, r rune, offset int) int {
+	if offset >= len(runes)-1 {
+		return -1
+	}
+	runes = runes[offset:]
+	idx := -1
+	for i := 0; i < len(runes); i++ {
+		if runes[i] == r {
+			idx = i
+			break
+		}
+	}
+	return idx + offset
 }
